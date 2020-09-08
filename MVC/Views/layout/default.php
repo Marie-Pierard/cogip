6a987c4099abb9e3aeb0dd380909be65676299b8
Gerardo Cella <becode@gerardo-cella.net>
2020-09-08 14:46:01 +0200

    maj include js resolution bug

--- MVC/Views/layout/default.php
+++ MVC/Views/layout/default.php
@@ -42,15 +42,9 @@
         <?= $content ?>
     </div>
 
-    <script src="/assets/js/jquery-3.5.1.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
-    </script>
-    <script src="/assets/js/popper.min.js"
-        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
-    </script>
-    <script src="/assets/js/bootstrap.min.js"
-        integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
-    </script>
+    <script src="/assets/js/jquery-3.5.1.slim.min.js"></script>
+    <script src="/assets/js/popper.min.js"></script>
+    <script src="/assets/js/bootstrap.min.js"></script>
 </body>
 
 </html>
