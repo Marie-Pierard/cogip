413fdff4c27198260fec1ac194affe2ad36cfb92
Gerardo Cella <becode@gerardo-cella.net>
2020-09-09 15:59:18 +0200

    Correction affiche page d'acceuil

--- MVC/Controllers/MainController.php
+++ MVC/Controllers/MainController.php
@@ -2,17 +2,10 @@
 
 namespace Cogit\Controllers;
 
-use Cogit\Core\Db;
-
 class MainController extends Controller
 {
     public function index()
     {
-        if(isset($_SESSION['user'])){
-            $this->render('main/index');
-        } else {
-            header('Location: /users/login');
-            exit;
-        }
+        $this->render('main/index');
     }
 }
