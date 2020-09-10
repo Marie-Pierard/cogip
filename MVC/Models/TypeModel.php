9d9f7a584fb2adc6e53e66b3a4308acca30fd00b
Marie Pierard <pierardmarie@gmail.com>
2020-09-10 15:22:48 +0200

    TypeModel

---
+++ MVC/Models/TypeModel.php
@@ -0,0 +1,53 @@
+<?php
+namespace Cogit\Models;
+
+class TypeModel extends Model
+{
+    protected $id;
+    protected $type;
+
+    public function __construct()
+    {
+        $this->table = 'cogit_Type';
+    }
+
+    /**
+     * Obtenir la valeur de Id
+     */ 
+    public function getId():int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Définir la valeur de Type
+     *
+     * @return  self
+     */ 
+    public function setType(string $type):self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Obtenir la valeur de Type
+     */ 
+    public function getType():string
+    {
+        return $this->type;
+    }
+
+    /**
+     * Définir la valeur de Id
+     *
+     * @return  self
+     */ 
+    public function setId(int $id):self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+}
