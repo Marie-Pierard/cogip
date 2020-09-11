4f7c163264fe658ebc39c75e10d5bebe54e77272
guide-charon <ch.vdld.dev@gmail.com>
2020-09-11 15:55:41 +0200

    second try

---
+++ MVC/Controllers/InvoicesController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Cogit\Controllers;
+use Cogit\Core\Form;
+use Cogit\Models\InvoiceModel;
+
+class InvoicesController extends Controller
+{
+    public function index()
+    {
+        $info = [
+            'invoice' => (new InvoiceModel())->findAll()
+        ];
+
+        $invoice = [];
+        foreach ($info as $value) {
+            $invoice[] = (new InvoiceModel())->hydrate($value)->join();
+        }
+        $info['invoice'] = $invoice;
+        
+        $this->render('invoices/invoice', $invoice);
+    }
+}
