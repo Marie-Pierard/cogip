169b131f6acae3fc7e3694fa924c2b33e871ba47
guide-charon <ch.vdld.dev@gmail.com>
2020-09-11 15:26:54 +0200

    add invoice view

---
+++ MVC/Views/invoices/invoice.php
@@ -0,0 +1,21 @@
+<h1 class="text-center">COGIT: Invoices List</h1>
+<table class="table mt-3">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">Invoice Number</th>
+      <th scope="col">Dates</th>
+      <th scope="col">Companies</th>
+      <th scope="col">Type</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php foreach($invoice as $line) : ?>
+        <tr>
+          <td><?= $line->getNumberInvoice() ?></td>
+          <td><?= $line->getDate() ?></td>
+          <td><?= $line->getCompany()->getName() ?></td>
+          <td><?= $line->getCompany()->getType() ?></td>
+        </tr>
+    <?php endforeach; ?>
+  </tbody>
+</table>
