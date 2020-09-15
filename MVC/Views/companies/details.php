7f9f1662427bdd411135ad62efe0fb5c47e35f6d
Gerardo Cella <becode@gerardo-cella.net>
2020-09-15 15:59:06 +0200

    details delete campagnie et invoice

--- MVC/Views/companies/details.php
+++ MVC/Views/companies/details.php
@@ -21,6 +21,9 @@
       <th scope="col">Name</th>
       <th scope="col">Phone</th>
       <th scope="col">Email</th>
+      <?php if(isset($_SESSION['user']['role']) && $_SESSION['user']['role'] === 'admin'): ?>
+        <th scope="col"></th>
+      <?php endif;?>
     </tr>
   </thead>
   <tbody>
@@ -29,6 +32,9 @@
             <td><a href="/contacts/details/<?= $line->id ?>"><?= $line->LastName . ' ' . $line->FirstName?></a></td>
             <td><?= $line->Phone ?></td>
             <td><?= $line->Email ?></td>
+            <?php if(isset($_SESSION['user']['role']) && $_SESSION['user']['role'] === 'admin'): ?>
+                <td><a href="/contacts/delete/<?= $line->id ?>"><img src="/assets/images/delete.png" alt="Delete invoice" class="icone"></a></td>
+            <?php endif;?>
         </tr>
     <?php endforeach; ?>
   </tbody>
