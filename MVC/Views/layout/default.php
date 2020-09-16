f2f171700a13fae825507afd4b6f17dd47c2ee93
Duquaine Simon <duquaine.simon@gmail.com>
2020-09-16 13:47:10 +0200

    ok

--- MVC/Views/layout/default.php
+++ MVC/Views/layout/default.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cogip Challenge</title>
-
+    <link rel="icon" href="/assets/images/logocogip.png">
     <link rel="stylesheet" href="/assets/css/style.css">
 </head>
 
